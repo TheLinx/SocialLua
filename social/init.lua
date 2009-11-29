5830c1b305b197339faaf798730b092f1400e8c4
Linus Sjögren <thelinx@unreliablepollution.net>
2009-11-29 21:10:54 +0100

    added basic request function

--- social/init.lua
+++ social/init.lua
@@ -4,3 +4,9 @@
 -- @author Bart van Strien (bart.bes@gmail.com)
 -- @author Linus Sjögren (thelinxswe@gmail.com)
 module("social", package.seeall) -- seeall for now
+
+function request(header, host, page, ...)
+    local d = string.format(header, host, page, ...)
+    -- send d
+    return
+end
