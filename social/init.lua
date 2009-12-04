36603b76692be6bc532e39dbc5e7fcf5571ed388
Linus Sjögren <thelinx@unreliablepollution.net>
2009-12-04 19:13:37 +0100

    added delete(url, auth)

--- social/init.lua
+++ social/init.lua
@@ -13,8 +13,6 @@ module("social")
 
 --- Makes a request.
 -- This is a back-end function.
--- @see get
--- @see post
 function request(method, url, auth, data)
 	local out = {}
     local r,c,h = http.request{
@@ -32,7 +30,6 @@ end
 
 --- Makes a GET request.
 -- Automatically makes a GET request with the given data.
--- @see post
 function get(url, auth)
 	local r,d,h,c = request("get", url, auth)
 	if not r then
@@ -43,9 +40,18 @@ end
 
 --- Makes a POST request.
 -- Automatically makes a POST request with the given data.
--- @see get
 function post(url, data, auth)
 	local r,d,h,c = request("post", url, auth, data)
+	if not r then
+		return false,h.status
+	end
+	return true,d,h,c
+end
+
+--- Makes a DELETE request.
+-- Automatically makes a DELETE request with the given data.
+function delete(url, auth)
+	local r,d,h,c = request("delete", url, auth)
 	if not r then
 		return false,h.status
 	end
