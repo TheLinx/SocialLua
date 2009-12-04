1cc4f6979a54ad04def24259ed5c8bd761d7b271
Linus Sjögren <thelinx@unreliablepollution.net>
2009-12-04 20:28:42 +0100

    added twitter.client:userFollowingLists(user, arg)

--- social/twitter.lua
+++ social/twitter.lua
@@ -336,7 +336,7 @@ function client:listTweets(name, user, arg)
 	return check(s,d,h,c)
 end
 
---- Checks what groups a user is member of.
+--- Checks what lists a user is in.
 -- @param user Target user. Defaults to the currently authed user.
 -- @param cursor Used for pagination.
 -- @return boolean Success or not.
@@ -344,6 +344,17 @@ end
 function client:userInLists(user, cursor)
 	if not self.authed then return false,"You must be logged in to do this!" end
 	local s,d,h,c = social.get(full("1/"..(user or self.username).."/lists/memberships", "api", {cursor = cursor}), self.auth)
+	return check(s,d,h,c)
+end
+
+--- Checks what lists a user is following.
+-- @param user Target user. Defaults to the currently authed user.
+-- @param cursor Used for pagination.
+-- @return boolean Success or not.
+-- @return unsigned If success, the statuses, if fail, the error message.
+function client:userFollowingLists(user, cursor)
+	if not self.authed then return false,"You must be logged in to do this!" end
+	local s,d,h,c = social.get(full("1/"..(user or self.username).."/lists/subscriptions", "api", {cursor = cursor}), self.auth)
 	return check(s,d,h,c)
 end
 
