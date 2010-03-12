b1aa015829dd6606bacbd04e6f33197f91114ae7
Linus Sjögren <thelinx@unreliablepollution.net>
2010-03-12 11:06:12 +0100

    updated unu module to the new return value spec

--- social/unu.lua
+++ social/unu.lua
@@ -11,15 +11,13 @@ module("social.unu")
 
 --- Shortens a URL with the u.nu service.
 -- @param u Input URL
--- @return boolean Success or not
--- @return unsigned If success, the shortened URL, if fail, the error message.
 function shorten(u)
 	assert(type(u) == "string", "bad argument #1 to 'shorten' (string expected, got "..type(u)..")")
 	local s,d = social.get("http://u.nu/unu-api-simple", {url = u})
 	if not s then return false,d end
 	d = d:gsub("\n", "")
 	if d:find("|") then
-		return false,d:sub(d:find("|")+1)
+		return nil,d:sub(d:find("|")+1)
 	end
-	return true,d
+	return d
 end
